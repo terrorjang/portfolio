774775060f695c44b7fb5dafa43ff9ca4759725c
youngseok.jang <ys123.jang@gmail.com>
2025-05-10 08:55:16 +0900

    chore: dev container setup

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<string> {
+    const password = 'password123';
+    const hashed = await bcrypt.hash(password, 10);
+    const match = await bcrypt.compare(password, hashed);
+
+    return match ? `✅ Password match: ${hashed}` : '❌ Password mismatch';
   }
 }
